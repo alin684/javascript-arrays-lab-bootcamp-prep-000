14d1a68b115ed6f9efb5f3c4dc5d6220a1246b5a
Abraham Lin <al684@cornell.edu>
2017-12-05 20:31:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const app = "I don't do much."
 
-function destructivelyAppendKitten(array1, kitten1) {
-  array1.push(kitten1);
+function destructivelyAppendKitten(kitten1) {
+  window.kittens.push(kitten1);
   return array1;
 }
