0a0ace3a9302ef1fff75e7beffb51c0aeeec4a11
Abraham Lin <al684@cornell.edu>
2017-12-05 20:33:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,3 +4,8 @@ function destructivelyAppendKitten(kitten1) {
   kittens.push(kitten1);
   return kittens;
 }
+
+function destructivelyPrependKitten(kitten2) {
+  kittens.unshift(kitten2);
+  return kittens;
+}
